aa438a6b84da8660ab87a14d8d9a3e175d04216a
agandia <aitor_gv@hotmail.com>
2026-03-02 13:52:26 +0100

    replaced string handling in shader loader

--- LearnWebGPU.js
+++ LearnWebGPU.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpe0zh4rad.js
+// include: /tmp/tmpf7ef0har.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -228,21 +228,21 @@ Module['FS_createPath']("/mnt/c/Users/Aitor/source/repos/LearnWebGPU/LearnWebGPU
 
   })();
 
-// end include: /tmp/tmpe0zh4rad.js
-// include: /tmp/tmpshdvhei1.js
+// end include: /tmp/tmpf7ef0har.js
+// include: /tmp/tmped_dycto.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpshdvhei1.js
-// include: /tmp/tmp1sfrsvia.js
+  // end include: /tmp/tmped_dycto.js
+// include: /tmp/tmph2gwddz7.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp1sfrsvia.js
+  // end include: /tmp/tmph2gwddz7.js
 
 
 // Sometimes an existing Module object exists with properties
