f73a8c9b15e0307a8d461d8d14971d04023caede
agandia <aitor_gv@hotmail.com>
2026-03-03 09:28:10 +0100

    Fixed memory leak with depth textures

--- LearnWebGPU.js
+++ LearnWebGPU.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmppde93s5u.js
+// include: /tmp/tmp3xx96xu2.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -228,21 +228,21 @@ Module['FS_createPath']("/mnt/c/Users/Aitor/source/repos/LearnWebGPU/LearnWebGPU
 
   })();
 
-// end include: /tmp/tmppde93s5u.js
-// include: /tmp/tmp8b95z_je.js
+// end include: /tmp/tmp3xx96xu2.js
+// include: /tmp/tmpjxag1y32.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp8b95z_je.js
-// include: /tmp/tmp75f7r9uh.js
+  // end include: /tmp/tmpjxag1y32.js
+// include: /tmp/tmpb8kdn4a4.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp75f7r9uh.js
+  // end include: /tmp/tmpb8kdn4a4.js
 
 
 // Sometimes an existing Module object exists with properties
